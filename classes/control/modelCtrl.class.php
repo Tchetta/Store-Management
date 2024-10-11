70ee5e66865a8816fa30696ee8fbae47950bd3df
Tchetta <franktchtta54@gmail.com>
2024-10-11 08:24:11 +0100

    model, brand and category quantity

--- classes/control/modelCtrl.class.php
+++ classes/control/modelCtrl.class.php
@@ -1,6 +1,57 @@
 <?php
 require_once('../includes/class_autoloader.inc.php');
 
+class Model extends Dbh {
+    // Method to add a new model
+    protected function addModel($modelId, $modelName, $brandId, $numberOfPorts=null, $portTypes=null, $powerRating=null, $quantity = null) {
+        $sql = "INSERT INTO model (model_id, model_name, number_of_ports, port_types, power_rating, brand_id, quantity) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$modelId, $modelName, $numberOfPorts, $portTypes, $powerRating, $brandId, $quantity]);
+    }
+
+    // Method to get a model by ID
+    protected function getModelById($modelId) {
+        $sql = "SELECT * FROM model WHERE model_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$modelId]);
+        return $stmt->fetch();
+    }
+
+    // Method to get all models
+    protected function getAllModels() {
+        $sql = "SELECT * FROM model";
+        $stmt = $this->connect()->query($sql);
+        return $stmt->fetchAll();
+    }
+
+    // Method to update model details
+    protected function updateModel($modelId, $modelName, $numberOfPorts, $portTypes, $powerRating, $brandId, $quantity) {
+        $sql = "UPDATE model SET model_name = ?, number_of_ports = ?, port_types = ?, power_rating = ?, brand_id = ?, quantity = ? WHERE model_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$modelName, $numberOfPorts, $portTypes, $powerRating, $brandId, $quantity, $modelId]);
+    }
+
+    // Method to delete a model by ID
+    protected function deleteModel($modelId) {
+        $sql = "DELETE FROM model WHERE model_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$by, $modelId]);
+    }
+
+    // Decrease the quantity of a model by 1
+    public function decreaseQuantity($modelId, $by) {
+        $sql = "UPDATE model SET number_of_ports = number_of_ports - ? WHERE model_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$by, $modelId]);
+    }
+    // Increase the quantity of a model by 1
+    public function increaseQuantity($modelId, $by) {
+        $sql = "UPDATE model SET number_of_ports = number_of_ports + ? WHERE model_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$modelId, $by]);
+    }
+}
+
 class ModelCtrl extends Model {
     // Create a new model
     public function createModel($modelId, $modelName, $numberOfPorts, $portTypes, $powerRating, $brandId, $quantity) {
@@ -35,16 +86,24 @@ class ModelCtrl extends Model {
         // Other existing methods...
     
         // Decrease model quantity
-        public function decreaseQuantity($modelId) {
+        public function decreaseQuantity($modelId, $by = 1) {
             // You might want to add some checks here, such as verifying if the quantity is greater than 0
-            $this->model->decreaseQuantity($modelId);
+            parent::increaseQuantity($modelId);
+
+            $brandCtrl = new BrandCtrl();
+            $categoryCtrl = new productCategoryCtrl();
+
+            $brandId = $this->getBrandIdByModel($modelId);
+            $categoryId = $this->getCategoryIdByModel($modelId);
+
+            $brandCtrl->updateBrandQuantity($brandId);
+            $categoryCtrl->updateCategoryQuantity($categoryId);
         }
 
-        public function increaseQuantity($modelId, $quantity) {
-            // Increment the quantity of the model by the input quantity
-            $sql = "UPDATE model SET quantity = quantity + ? WHERE model_id = ?";
-            $stmt = $this->connect()->prepare($sql);
-            $stmt->execute([$quantity, $modelId]);
+        public function increaseQuantity($modelId, $by = 1) {
+            // You might want to add some checks here, such as verifying if the quantity is greater than 0
+            parent::increaseQuantity($modelId);
+            
         }
     
         public function getBrandIdByModel($modelId) {
@@ -54,5 +113,14 @@ class ModelCtrl extends Model {
             $stmt->execute([$modelId]);
             return $stmt->fetchColumn();
         }
+
+        public function getCategoryIdByModel($modelId) {
+            // Get the brand_id for the given model
+            $brandId = $this->getBrandIdByModel($modelId);
+            $brand = new BrandCtrl();
+            return $brand->getCategoryIdByBrand($brandId);
+        }
+
+        
     
 }
--- classes/control/productCategoryCtrl.class.php
+++ classes/control/productCategoryCtrl.class.php
@@ -1,6 +1,79 @@
 <?php
 require_once('../includes/class_autoloader.inc.php');
 
+class ProductCategory extends Dbh {
+    // Method to add a new product category
+    public function addCategory($categoryName) {
+        if (empty($categoryName)) {
+            throw new Exception("Name is required.");
+        }
+        
+        $sql = "INSERT INTO product_category (category_name) VALUES (?)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$categoryName]);
+    }
+
+    // Method to get a category by ID
+    public function getCategoryById($categoryId) {
+        $sql = "SELECT * FROM product_category WHERE category_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$categoryId]);
+        return $stmt->fetch();
+    }
+
+    // Method to get all categories
+    public function getAllCategories() {
+        $sql = "SELECT * FROM product_category";
+        $stmt = $this->connect()->query($sql);
+        return $stmt->fetchAll();
+    }
+
+    // Method to update category details
+    public function updateCategory($categoryId, $categoryName, $quantity) {
+        $sql = "UPDATE product_category SET category_name = ?, quantity = ? WHERE category_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$categoryName, $quantity, $categoryId]);
+    }
+
+    // Method to update category details
+    public function setCategoryName($categoryId, $categoryName) {
+        $sql = "UPDATE product_category SET category_name = ?WHERE category_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$categoryName, $categoryId]);
+    }
+
+    // Method to delete a category by ID
+    public function deleteCategory($categoryId) {
+        $sql = "DELETE FROM product_category WHERE category_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$categoryId]);
+    }
+
+    // Decrease the quantity of a product category by 1
+    public function decreaseQuantity($categoryId, $by) {
+        $sql = "UPDATE product_category SET quantity = quantity - ? WHERE category_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$categoryId, $by]);
+    }
+    public function increaseQuantity($categoryId, $by) {
+        $sql = "UPDATE product_category SET quantity = quantity + ? WHERE category_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$categoryId, $by]);
+    }
+    public function updateCategoryQuantity($categoryId) {
+        // Sum the quantity of all brands belonging to this category
+        $sql = "SELECT SUM(quantity) AS total_quantity FROM brand WHERE category_id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$categoryId]);
+        $totalQuantity = $stmt->fetchColumn();
+
+        // Update the category's quantity
+        $sqlUpdate = "UPDATE product_category SET quantity = ? WHERE category_id = ?";
+        $stmtUpdate = $this->connect()->prepare($sqlUpdate);
+        $stmtUpdate->execute([$totalQuantity, $categoryId]);
+    }
+}
+
 class ProductCategoryCtrl extends ProductCategory {
 
     // Create a new category
@@ -33,23 +106,17 @@ class ProductCategoryCtrl extends ProductCategory {
         // Other existing methods...
     
         // Decrease product category quantity
-        public function decreaseQuantity($categoryId) {
+        public function decreaseQuantity($categoryId, $by = 1) {
             // You might want to add some checks here, such as verifying if the quantity is greater than 0
-           parent::decreaseQuantity($categoryId);
+           parent::decreaseQuantity($categoryId, $by = 1);
+        }
+        // Increase product category quantity
+        public function increaseQuantity($categoryId, $by = 1) {
+            // You might want to add some checks here, such as verifying if the quantity is greater than 0
+           parent::increaseQuantity($categoryId, $by = 1);
         }
 
-        public function updateCategoryQuantity($categoryId) {
-            // Sum the quantity of all brands belonging to this category
-            $sql = "SELECT SUM(quantity) AS total_quantity FROM brand WHERE category_id = ?";
-            $stmt = $this->connect()->prepare($sql);
-            $stmt->execute([$categoryId]);
-            $totalQuantity = $stmt->fetchColumn();
-    
-            // Update the category's quantity
-            $sqlUpdate = "UPDATE product_category SET quantity = ? WHERE category_id = ?";
-            $stmtUpdate = $this->connect()->prepare($sqlUpdate);
-            $stmtUpdate->execute([$totalQuantity, $categoryId]);
-        }
+        
     
 }
 
