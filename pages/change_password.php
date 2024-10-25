a57a9077f16d32a1dbb6507f5ffb1e0532825307
Genevieve12-tech <asuetta123@gmail.com>
2024-10-25 12:23:55 +0100

    style the change password

--- pages/change_password.php
+++ pages/change_password.php
@@ -4,26 +4,54 @@ if(isset($_GET['id'])) {
 } else {
     header("Location: dashboard.php?page=edit_user&error=no+user+selected+to+change+password");
 }
-
 ?>
+<div class="model_container model_mt-5">
+    <form action="../includes/change_password.inc.php" method="post">
+        <input type="hidden" name="user_id">
+        
+        <div class="model_form-group password-container">    
+            <label for="old_password">Old Password:</label>
+            <input type="password" id="old_password" name="old_password">
+            <span class="eye-icon" onclick="togglePassword('old_password', 'eyeIconOld')">
+                <i class="fas fa-eye" id="eyeIconOld"></i>
+            </span>
+        </div>
 
-<form action="../includes/change_password.inc.php" method="post">
+        <div class="model_form-group password-container">
+            <label for="new_password">New Password:</label>
+            <input type="password" id="new_password" name="new_password" required>
+            <span class="eye-icon" onclick="togglePassword('new_password', 'eyeIconNew')">
+                <i class="fas fa-eye" id="eyeIconNew"></i>
+            </span>
+        </div>
 
-<input type="hidden" name="user_id">
+        <div class="model_form-group password">
+            <label for="confirm_password">Confirm New Password:</label>
+            <input type="password" id="confirm_password" name="confirm_password">
+        </div>
+        <button type="submit">Submit</button>
+    </form>
 
-<div class="model_form-group password">    
-    <label for="old_password">Old Password:</label>
-    <input type="password" id="old_password" name="old_password">
+    <div class="back-arrow-container">
+        <a href="javascript:history.back()" class="back-arrow">
+            &#8592; Back
+        </a>
+    </div>
 </div>
 
-<div class="model_form-group password">
-    <label for="new_password">New Password:</label>
-    <input type="password" id="new_password" name="new_password">
-</div>
+<script>
+function togglePassword(fieldId, iconId) {
+    var passwordField = document.getElementById(fieldId);
+    var eyeIcon = document.getElementById(iconId);
 
-<div class="model_form-group password">
-    <label for="confirm_password">Confirm New Password:</label>
-    <input type="password" id="confirm_password" name="confirm_password">
-</div>
-<button type="submit">Submit</button>
-</form>
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordField.type = "password";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
+</script>
